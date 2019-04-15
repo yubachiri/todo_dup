9ad23c190d9b03037c25ddaa46e79fbbd9ffa46e
kakimoso <k.i.moso9532@gmail.com>
2019-04-15 18:54:30 +0900

    apolloでtask.allまで

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -1,10 +1,15 @@
 module Types
   class QueryType < Types::BaseObject
+    field :all_task, [TaskType], null: false, description: "return all task"
     field :first_task, TaskType, null: false, description: "return first task"
     field :task_find_by, TaskType, null: false, description: "return a task" do
       argument :task_id, Integer, required: true
     end
     field :resolver_method_exam, String, null: false, description: "this is resolver sample", resolver_method: :say_hello
+
+    def all_task
+      Task.all
+    end
 
     def first_task
       Task.first
--- app/javascript/packs/vue_apollo_sample.js
+++ app/javascript/packs/vue_apollo_sample.js
@@ -1,10 +1,51 @@
 import Vue from 'vue/dist/vue.esm.js'
+import ApolloClient from "apollo-client"
+import {HttpLink} from 'apollo-link-http'
+import {InMemoryCache} from 'apollo-cache-inmemory';
+import {ApolloLink, concat} from 'apollo-link';
+import gql from 'graphql-tag'
+
+const httpLink = new HttpLink({uri: "http://localhost:3008/graphql"});
+
+const authMiddleware = new ApolloLink((operation, forward) => {
+  operation.setContext({
+    headers: {
+      'X-CSRF-Token': document.querySelector('meta[name=csrf-token]').getAttribute('content'),
+    },
+  });
+  return forward(operation);
+});
+
+const apolloClient = new ApolloClient({
+  link: concat(authMiddleware, httpLink),
+  cache: new InMemoryCache(),
+});
 
 document.addEventListener('DOMContentLoaded', () => {
+  const TASK_QUERY = gql`
+  query allTask{
+    allTask {
+      id
+      title
+      status
+      priority
+    }
+  }`
+
   const app = new Vue({
     el: '#mount_target',
+    async mounted() {
+      self = this;
+      apolloClient.query({
+        query: TASK_QUERY
+      })
+        .then(function (result) {
+          self.allTask = result.data.allTask;
+        })
+        .catch(error => console.log(error));
+    },
     data: {
-      message: 'hello, vue'
+      allTask: [],
     }
   })
 })
